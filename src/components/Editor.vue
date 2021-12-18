4b1717ced4449fe478c5dde53d3a3b518965fdaa
xzbbsuir <494588788@qq.com>
2021-12-18 15:05:37 +0800

    修复，上传编辑器文件同步到自建oss

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -6,9 +6,10 @@
 
 <script>
 	import E from 'wangeditor';
-	const OSS = require('ali-oss');
+	// const OSS = require('ali-oss');
 	import { v4 as uuidv4 } from 'uuid';
-	const key=require("../config/key.js")
+	// const key=require("../config/key.js")
+	const Minio = require('minio')
 	//uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 	export default {
 		name: 'editor',
@@ -21,7 +22,7 @@
 			};
 		},
 		watch: {
-			editorContent(old, v) {
+			editorContent() {
 				this.$emit('val', this.editorContent);
 
 			},
@@ -42,32 +43,56 @@
 				this.editorContent = html;
 
 			};
+			var minioClient = new Minio.Client({
+				endPoint: 'wx.moono.vip',
+				port: 8999,
+				useSSL: true,
+				accessKey:  '111111111',
+				secretKey: '111111111'
+			});
 			
 			// 具体值需要去阿里云控制台获取
-			let client = new OSS({
-			  // // region以杭州为例（oss-cn-hangzhou），其他region按实际情况填写。
-			  region: 'oss-cn-hangzhou',
-			  // // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-			accessKeyId: key.accessKeyId,
-			accessKeySecret: key.accessKeySecret,
-			  bucket: 'bsuir',
-			});
+			// let client = new OSS({
+			//   // // region以杭州为例（oss-cn-hangzhou），其他region按实际情况填写。
+			//   region: 'oss-cn-hangzhou',
+			//   // // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
+			// accessKeyId: key.accessKeyId,
+			// accessKeySecret: key.accessKeySecret,
+			//   bucket: 'bsuir',
+			// });
 			var n
 			editor.config.customUploadImg = function (resultFiles, insertImgFn) {
-			    // resultFiles 是 input 中选中的文件列表
-			    // insertImgFn 是获取图片 url 后，插入到编辑器的方法
-				let id=uuidv4()
+                // resultFiles 是 input 中选中的文件列表
+                // insertImgFn 是获取图片 url 后，插入到编辑器的方法
+				console.log(resultFiles[0]);
+                    let objectkey = "zbr/" + uuidv4() + resultFiles[0].name
+					let fr = new FileReader();//用FileReader 读取文件流
+					fr.readAsArrayBuffer(resultFiles[0]);
+					fr.onload = function(ex) {
+					minioClient.putObject('mynumber1', objectkey, Buffer.from(ex.target.result), resultFiles[0].size,function(err) {
+											if(err) {
+												return console.log(err); // err should be null
+											}
+											sessionStorage.imgid+=n
+											let last = 'https://wx.moono.vip:8999/mynumber1/' + objectkey
+											insertImgFn(last)
+											// console.log("Success", objInfo);
+											self.$emit("toFatherData", last)
+						})
+					}
+
+				
 			
-			  
-			    client.put('myImg/any/'+id, resultFiles[0])
-			      .then(function (res) {
-			        // 上传图片，返回结果，将图片插入到编辑器中
-					  sessionStorage.imgid+=n
-			        insertImgFn(res.url)
-					self.$emit("toFatherData", res.url)
-			      }).catch(function (err) {
-			        console.log(err)
-			      })
+ 
+						// client.put('myImg/any/'+id, resultFiles[0])
+						//   .then(function (res) {
+						//     // 上传图片，返回结果，将图片插入到编辑器中
+						// 	  sessionStorage.imgid+=n
+						//     insertImgFn(res.url)
+						// 	self.$emit("toFatherData", res.url)
+						//   }).catch(function (err) {
+						//     console.log(err)
+						//   })
 			}
 
 			editor.create();
