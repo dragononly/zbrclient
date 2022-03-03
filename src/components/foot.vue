a5b1fac5efaee2b40dff3b21fabfd101ef9a0cd4
xzbbsuir <494588788@qq.com>
2022-03-03 12:55:48 +0800

    小优化

--- src/components/foot.vue
+++ src/components/foot.vue
@@ -1,32 +1,37 @@
 <template>
-	<div class="max">
-		<div>杭州金星铜工程有限公司</div>
-		<div style="margin-top: 15px;">版权所有：杭州金星铜工程有限公司© 
-		网站备案 / 许可证号：<a style="color: #c4c2c6;" href="https://beian.miit.gov.cn/">浙ICP备06034011号-1© </a></div>
-	</div>
+  <div class="max">
+    <div>杭州金星铜工程有限公司</div>
+    <div style="margin-top: 15px">
+      版权所有：杭州金星铜工程有限公司© 网站备案 / 许可证号：<a
+        style="color: #c4c2c6"
+        href="https://beian.miit.gov.cn/"
+        >浙ICP备19014233号-1©
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'foot',
+  name: 'foot',
 
-	data() {
-		return {};
-	},
-	props: {
-		msg: String
-	}
+  data() {
+    return {};
+  },
+  props: {
+    msg: String,
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .max {
-	background: #727278;
-	padding: 30px;
-	color: #c4c2c6;
-	font-weight: 500;
-	font-size: 14px;
-	clear: both;
+  background: #727278;
+  padding: 30px;
+  color: #c4c2c6;
+  font-weight: 500;
+  font-size: 14px;
+  clear: both;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,10 +7,21 @@ import store from './store'
 import { Lazyload } from 'vant';
 import { DatePicker } from 'ant-design-vue';
 
+import SocketIO from "socket.io-client"
 import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import VueSocketIO from 'vue-socket.io'
+const socketOptions = {
+  autoConnect: true,       // 自动连接     这里为我项目需求  需要在指定情况下才连接socket
+}
+Vue.use(
+  new VueSocketIO({
+    debug: false ,   // debug调试，生产建议关闭
+    connection: SocketIO("http://127.0.0.1:9000", socketOptions),
+  })
+)
 
 
 let g = {}
@@ -33,6 +44,21 @@ Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
+   // 这里为全局监听socket事件消息，监听函数这里只写了一点，其实很有很多事件。
+   sockets: {
+    connecting() {
+      console.log('正在连接')
+    },
+    disconnect() {
+      console.log("Socket 断开");
+    },
+    connect_failed() {
+      console.log('连接失败')
+    },
+    connect() {
+      console.log('socket connected')
+    }
+  },
   router,
   store,
   render: h => h(App)
